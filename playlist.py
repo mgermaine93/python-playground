eca256451c7c1cb669ee16a44d1a7c35cb75ed55
mgermaine93 <mgermaine93@gmail.com>
2020-03-27 14:29:07 -0400

    First commit

---
+++ playlist.py
@@ -0,0 +1,33 @@
+# Practice constructing a nested dictionary data structure
+
+playlist = {
+    'title': 'Live At Luther College',
+    'author': 'Matt Germaine',
+    'year_released': 1999,
+    'genre': 'Rock',
+    'songs': [
+        {'title': 'One Sweet World', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '5:43', 'disk': 1},
+        {'title': '#41', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '5:38', 'disk': 1},
+        {'title': 'Tripping Billies', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '5:49', 'disk': 1},
+        {'title': 'Jimi Thing', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '8:13', 'disk': 1},
+        {'title': 'Satellite', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '4:40', 'disk': 1},
+        {'title': 'Crash Into Me', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '5:33', 'disk': 1},
+        {'title': 'Deed Is Done', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '4:52', 'disk': 1},
+        {'title': 'Lover Lay Down', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '5:34', 'disk': 1},
+        {'title': 'What Would You Say', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '5:07', 'disk': 1},
+        {'title': 'Minarets', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '7:01', 'disk': 1},
+        {'title': 'Cry Freedom', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '5:39', 'disk': 1},
+        {'title': 'Dancing Nancies', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '7:15', 'disk': 1},
+        {'title': 'Typical Situation', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '7:02', 'disk': 2},
+        {'title': 'Stream', 'artist': ['Tim Reynolds'], 'duration': '5:50', 'disk': 2},
+        {'title': 'Warehouse', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '9:17', 'disk': 2},
+        {'title': 'Christmas Song', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '5:25', 'disk': 2},
+        {'title': 'Seek Up', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '7:43', 'disk': 2},
+        {'title': 'Say Goodbye', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '5:20', 'disk': 2},
+        {'title': 'Ants Marching', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '5:27', 'disk': 2},
+        {'title': 'Little Thing', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '6:19', 'disk': 2},
+        {'title': 'Halloween', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '3:00', 'disk': 2},
+        {'title': 'Granny', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '3:22', 'disk': 2},
+        {'title': 'Two Step', 'artist': ['Dave Matthews', 'Tim Reynolds'], 'duration': '6:35', 'disk': 2},
+    ]
+}
