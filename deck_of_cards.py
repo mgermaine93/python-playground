d37913b49cc46d3f9eeea272a626bbfa83a236ff
mgermaine93 <mgermaine93@gmail.com>
2020-04-14 17:07:41 -0400

    Added Deck of Cards exercise

---
+++ deck_of_cards.py
@@ -0,0 +1,64 @@
+# Import statements
+from random import shuffle
+
+# Defines the Card class
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
+
+class Deck:
+
+    # Initializes the deck
+    def __init__(self):
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+        # Uses list comprehension
+        self.cards = [Card(value, suit) for suit in suits for value in values]
+
+    # Returns the number of cards in the deck in the form of a string
+    def __repr__(self):
+        return f"Deck of {self.count()} cards"
+
+    # Gets the amount of cards in the deck
+    def count(self):
+        return len(self.cards)
+
+    # num = the number of cards we want to remove
+    def _deal(self, num):
+        count = self.count()
+        actualNumberToRemove = min([count, num])
+
+        # Accounts for a deck of zero cards
+        if count == 0:
+            raise ValueError("All cards have already been dealt!")
+        # Go "actualNumberToRemove" spaces back and remove the cards from there through the end
+        cards = self.cards[-actualNumberToRemove:]
+        # Redefines self.cards to omit the cards that were dealt
+        self.cards = self.cards[:-actualNumberToRemove]
+        return cards
+
+    def deal_cards(self):
+        # Returns a SINGLE card, not a card from a list
+        return self._deal(1)[0]
+
+    def deal_hand(self, hand_size):
+        return self._deal(hand_size)
+
+    def shuffle(self):
+        if self.count() < 52:
+            raise ValueError("Only full decks can be shuffled!")
+        # Shuffle method from above is implemented here
+        shuffle(self.cards)
+        return self
+    
+# Tests...
+deck = Deck()
+deck.shuffle()
+card = deck.deal_cards()
+print(card)
+hand = deck.deal_hand(5)
+print(hand)
