1b449548308abf8484179082b3299ed9233f0a82
mgermaine93 <mgermaine93@gmail.com>
2020-04-27 22:16:51 -0400

    finished first web scraper

--- first_scraping_program.py
+++ first_scraping_program.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
+from csv import writer
 
 # Gets the website's HTML
 response = requests.get("https://www.rithmschool.com/blog")
@@ -11,10 +11,20 @@ soup = BeautifulSoup(response.text, "html.parser")
 # Looks for all article tags within the website
 articles = soup.find_all("article")
 
-# Loop through each article...
-for article in articles:
+# "w" = write mode
+with open("blog_data.csv", "w") as csv_file:
+    csv_writer = writer(csv_file)
+    csv_writer.writerow("title", "link", "date")
 
-    # And get the text from each anchor tag
-    title = article.find("a").get_text())
-    href=article.find("a")
-    # date =
+    # Loop through each article...
+    for article in articles:
+        # And get the text from each anchor tag...
+        a_tag = article.find("a")
+        # title tag...
+        title = a_tag.get_text()
+        # href tag...
+        link = a_tag['href']
+        # and date
+        date = article.find("time")["datetime"]
+
+        csv_writer.writerow([title, link, date])
