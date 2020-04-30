44e7fa345ea972f2b070723a0c7137123c692825
mgermaine93 <mgermaine93@gmail.com>
2020-04-29 22:16:31 -0400

    Added HTML parser to BeautifulSoup Constructor

--- quote_scraping_project.py
+++ quote_scraping_project.py
@@ -1,9 +1,16 @@
-# http://quotes.toscrape.com
-
 import requests
 from bs4 import BeautifulSoup
 
+all_quotes = []
 res = requests.get("http://quotes.toscrape.com")
-soup = BeautifulSoup(res.text)
+soup = BeautifulSoup(res.text, features="html.parser")
 quotes = soup.find_all(class_="quote")
-print(quotes)
+for quote in quotes:
+    all_quotes.append({
+        # all text has class of "text"
+        "text": quote.find(class_="text").get_text(),
+        # all authors have class of "author"
+        "author": quote.find(class_="author").get_text()
+    })
+
+print(all_quotes)
