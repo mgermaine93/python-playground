cadb026a17c25fba45286f1537142f45322e3253
mgermaine93 <mgermaine93@gmail.com>
2020-10-23 17:15:52 -0400

    Continued adding to adventure game.

--- adventure-game.py
+++ adventure-game.py
@@ -1,8 +1,18 @@
 # Welcome message
-print("Choose your own adventure!")
+print("Choose your own adventure! ")
 
 # User adds their name
-name = input("What is your name?")
+name = input("What is your name? ")
 
 # User adds their age
-age = input("What is your age?")
+age = int(input("What is your age? "))
+
+print("Hello", name, "you are", age, "years old.")
+
+if age >= 18:
+    print("You are old enough to play!")
+    wants_to_play = input("Do you want to play? (yes/no) ")
+    if wants_to_play == "yes":
+        print("Let's play!")
+else:
+    print("Unfortunately, you are not old enough to play...")
