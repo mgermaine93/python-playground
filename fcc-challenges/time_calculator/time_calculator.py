ad0deb6319859d845d54f69a0e8f71e0ff02d099
mgermaine93 <mgermaine93@gmail.com>
2021-01-10 20:22:57 -0500

    Solved sleep_in.py.  Just need to add tests.

--- fcc-challenges/time_calculator/time_calculator.py
+++ fcc-challenges/time_calculator/time_calculator.py
@@ -12,17 +12,15 @@ def add_time(start, duration, day_of_week=False):
     duration_minutes = int(split_duration_time[1])
 
     hours = int(start_hours + duration_hours)
-    minutes = start_minutes + duration_minutes
+    minutes = int(start_minutes + duration_minutes)
 
     # Need to:
-          
-    
+
     # The function should add the duration time to the start time and return the result
     # If the result will be the next day, it should show (next day) after the time
     # If the result will be more than one day later, it should show (n days later) after the time, where "n" is the number of days later
     # If the function is given the optional starting day of the week parameter, then the output should display the day of the week of the result
     # The day of the week in the output should appear after the time and before the number of days later
-
 
 
     # print(hours)
@@ -37,5 +35,6 @@ def add_time(start, duration, day_of_week=False):
     # print(duration_hours + duration_minutes)
 
     return split_start_time
+
 
 add_time("3:30 PM", "2:12")
---
+++ python-coding-bat/warmup-1/sleep_in/test_module.py
@@ -0,0 +1,3 @@
+# sleep_in(False, False) → True
+# sleep_in(True, False) → False
+# sleep_in(False, True) → True
