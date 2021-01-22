8184a8dab915f6c8bb9ea9610b9d9309f02c78b4
mgermaine93 <mgermaine93@gmail.com>
2021-01-22 15:34:18 -0500

    Added make_bricks tests.

--- python-coding-bat/logic-2/make_bricks/make_bricks.py
+++ python-coding-bat/logic-2/make_bricks/make_bricks.py
@@ -1,14 +1,12 @@
 # We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and big bricks (5 inches each). Return True if it is possible to make the goal by choosing from the given bricks. This is a little harder than it looks and can be done without any loops.
 
 def make_bricks(small, big, goal):
-    # False if not enough big
-    # False if not enough small
     remainder = goal % 5
     total = (big * 5) + small
-    # Fail if not enough bricks, period
+    # Fail if not enough pieces, period
     if goal > total:
         return False
-    # Fail if not enough small bricks to make up the difference
+    # Fail if not enough small pieces to make up the difference
     if remainder > small:
         return False
     return True
