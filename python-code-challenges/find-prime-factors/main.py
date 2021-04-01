5175a53ef8ceb0cfa20bf5b345714db41e63b830
mgermaine93 <mgermaine93@gmail.com>
2021-04-01 12:40:07 -0400

    Removed unnecessary print statement.

--- python-code-challenges/find-prime-factors/main.py
+++ python-code-challenges/find-prime-factors/main.py
@@ -3,8 +3,5 @@ from find_prime_factors import find_prime_factors
 from unittest import main
 
 
-# print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
-
-
 # Run unit tests automatically
 main(module='test_module', exit=False)
